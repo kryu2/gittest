f6cacd85846a4466193370f97a6eb40cb751c63c
Satoru, Yamamoto <yama@dreamhive.co.jp>
2025-05-27 16:31:05 +0900

    Add PHP script to display current datetime

---
+++ current_datetime.php
@@ -0,0 +1,8 @@
+<?php
+
+// Display the current date and time in 'Y-m-d H:i:s' format
+
+$currentDateTime = date('Y-m-d H:i:s');
+
+// Output the result
+echo "Current date and time: $currentDateTime\n";
